f787f568f958ed9e083f89ee4196ee2e376c6ae2
mean pt <meanpt@gmail.com>
2017-12-19 08:11:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout (shoutIt){
+  return shoutIt.toUpperCase()
+}
+function whisper(whisperIt){
+  return whisperIt.toLowerCase()
+}
+function logShout (shoutIt) {
+  console.log(shoutIt.toUpperCase());
+}
+function logWhisper (whisperIt) {
+  console.log(whisperIt.toLowerCase());
+}
+function sayHiToGrandma(say){
+  var input = say ;
+  if (input = say.toLowerCase()){
+    return"I can't hear you!"
+  }
+}
+function sayHiToGrandma(say){
+  var input = say ;
+  if (input.toLowerCase() = "i love you, grandma."){
+  return "I love you, too."}
+  else if (input ) 
+  {
+    
+  }
+  
+  
+    say.toLowerCase()){
+    return "I can't hear you!"
+    }
+  else {
+    return "YES INDEED!"
+  }
+}
